aadab60999a934f7011979892c0957c45208f149
DukhiAtma <iamantarip@gmail.com>
2023-03-05 22:01:50 +0530

    removed font installation

--- install.sh
+++ install.sh
@@ -8,18 +8,15 @@ mkdir ~/Downloads && cd ~/Downloads
 wget https://github.com/dracula/gtk/archive/master.zip && unzip master.zip && sudo cp -r gtk-master /usr/share/themes/Dracula
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 
-mv .config ~/
-mkdir ~/.fonts && cd ~/.fonts && wget https://fonts.google.com/download?family=Anonymous%20Pro -O fonts.zip && unzip fonts.zip && rm fonts.zip && fc-cache
-
-cd ~/Downloads
+cp -r ~/dotfiles/.config/* ~/.config/
 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+chsh -s /bin/zsh
 
 cp ~/dotfiles/.zshrc ~/.zshrc
-cp -r ~/dotfiles/.config/* ~/.config/
+
 
 rm -rf ~/dotfiles
