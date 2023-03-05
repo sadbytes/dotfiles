e174c4b850ddaadf83f4811019452577d43dc6f8
DukhiAtma <iamantarip@gmail.com>
2023-03-05 12:06:09 +0530

    install script and config files added

---
+++ .zshrc
@@ -0,0 +1,11 @@
+export ZSH="$HOME/.oh-my-zsh"
+
+
+ZSH_THEME="gentoo"
+
+
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh
+
+alias ll='ls -la'
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt-get install -y thunar xfdesktop4 xfwm4 xfce4-panel xfce4-session xfce4-settings terminator xfconf  xfce4-power-manager xfce4-notifyd xfce4-clipman-plugin xfce4-whiskermenu-plugin curl firefox-esr unzip bzip2 zsh
+
+wget https://github.com/dracula/gtk/archive/master.zip && unzip master.zip && cp -r gtk-master /usr/share/themes/dracula
+wget -qO- https://git.io/papirus-icon-theme-install | sh
+git clone https://github.com/dracula/terminator && yes | ./terminator/install.sh
+mv .config ~/
+mkdir ~/.fonts && cd ~/.fonts && wget https://fonts.google.com/download?family=Anonymous%20Pro -O fonts.zip && unzip fonts.zip && rm fonts.zip && fc-cache
+
+cd ~/Downloads
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+cp dotfiles/.zshrc ~/.zshrc
+cp -r dotfiles/.config ~/.config
